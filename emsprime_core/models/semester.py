510b7f0898318ade56c895f08d8cfb4f3a64796f
Riyaj Pathan <rjpathan19@gmail.com>
2016-05-02 19:14:43 +0530

    1)add Course field in Semester (auto compute from Edition) 2)modify Semester list view

--- emsprime_core/models/semester.py
+++ emsprime_core/models/semester.py
@@ -33,6 +33,7 @@ class EmsSemester(models.Model):
     number= fields.Integer('Number')
     code = fields.Char('Code')
     edition_id = fields.Many2one('ems.edition', 'Edition', required=True)
+    course_id = fields.Many2one('ems.course', string='Course', related='edition_id.course_id', store=True, readonly=True)
     subject_ids = fields.Many2many('ems.subject', 'ems_semester_subject_rel', 'semester_id', 'subject_id', string='Subjects')
 
     @api.one
