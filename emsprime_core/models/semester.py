1b59967b9dc2ad514555eee0a725afc903cdc1dd
Riyaj Pathan <rjpathan19@gmail.com>
2016-06-14 16:10:47 +0530

    Semester view changes : add/remove fields

--- emsprime_core/models/semester.py
+++ emsprime_core/models/semester.py
@@ -28,15 +28,26 @@ class EmsSemester(models.Model):
 
     name = fields.Char('Name', size=32, required=False)
     start_date = fields.Date(
-        'Start Date', required=True, default=fields.Date.today())
-    end_date = fields.Date('End Date', required=True)
+        'Start Date', required=False, default=fields.Date.today())
+    end_date = fields.Date('End Date', required=False)
     number= fields.Integer('Number')
     code = fields.Char('Code')
     course_id = fields.Many2one('ems.course', 'Course', required=True)
     edition_id = fields.Many2one('ems.edition', 'Edition', required=True)
     #course_id = fields.Many2one('ems.course', string='Course', related='edition_id.course_id', store=True, readonly=True)
     subject_ids = fields.Many2many('ems.subject', 'ems_semester_subject_rel', 'semester_id', 'subject_id', string='Subjects')
-    semester = fields.Selection([('1', 'I'), ('2', 'II')], 'Semester')
+    subject_line = fields.One2many('ems.semester.subject', 'semester_id', 'Subjects')
+    semester = fields.Selection([('1', 'I'), 
+                                ('2', 'II'), 
+                                ('3', 'III'),
+                                ('4', 'IV'),
+                                ('5', 'V'),
+                                ('6', 'VI'),
+                                ('7', 'VII'),
+                                ('8', 'VIII'),
+                                ('9', 'IX')
+
+            ], 'Semester')
     year = fields.Char('Year', size=4)
 
     @api.one
@@ -47,5 +58,14 @@ class EmsSemester(models.Model):
         if start_date > end_date:
             raise ValidationError("End Date cannot be set before Start Date.")
 
+class EmsSemesterSubject(models.Model):
+    _name = "ems.semester.subject"
+    _description = "Semester Subjects"
+
+    semester_id = fields.Many2one('ems.semester', 'Semester')
+    subject_id = fields.Many2one('ems.subject', 'Subject')
+    week_work_load = fields.Float('CHS', help='Weekly Work Load')
+    student_contact = fields.Float('Contact', help='Hours of Contact with Student')
+    ects = fields.Integer('ECTS')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
