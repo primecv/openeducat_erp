25a4dff7dfedde9cc06a7fdd9e26fe5e7bff5203
Jerson Figueiredo <jerson.figueiredo@primeconsulting.org>
2016-06-22 12:48:49 -0100

    change state field to translatable

--- emsprime_core/models/student.py
+++ emsprime_core/models/student.py
@@ -138,7 +138,7 @@ class EmsStudent(models.Model):
                               ('submit', 'Submitted'),
                               ('received', 'Received'),
                               ('confirmed', 'Accepted'),
-                              ('rejected', 'Rejected')], 'State', default='draft', track_visibility='onchange')
+                              ('rejected', 'Rejected')], 'State', default='draft', track_visibility='onchange',translate=True)
     #Permanant Address fields:
     pstreet = fields.Char('Street')
     pstreet2 = fields.Char('Street2')
