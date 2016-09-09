680f5abdffad3887edb7554667f8dd92dba2cce7
Jerson Figueiredo <jerson.figueiredo@primeconsulting.org>
2016-09-09 09:31:27 -0100

    Change course plan

--- emsprime_core/models/course_plan.py
+++ emsprime_core/models/course_plan.py
@@ -40,13 +40,13 @@ class EmsCoursePlan(models.Model):
         default = dict(default or {}, name=_("%s - copy") % self.name)
         return super(EmsCoursePlan, self).copy(default=default)
 
-    @api.one
-    @api.constrains('start_date', 'end_date')
-    def check_dates(self):
-        start_date = fields.Date.from_string(self.start_date)
-        end_date = fields.Date.from_string(self.end_date)
-        if start_date > end_date:
-            raise ValidationError("End Date cannot be set before Start Date.")
+    #@api.one
+    #@api.constrains('start_date', 'end_date')
+    #def check_dates(self):
+    #    start_date = fields.Date.from_string(self.start_date)
+    #    end_date = fields.Date.from_string(self.end_date)
+    #    if start_date > end_date:
+    #        raise ValidationError("End Date cannot be set before Start Date.")
 
     @api.model
     def name_search(self, name, args=None, operator='ilike', limit=100):
