ef19b3902c5eaf2d8d77ef00e3da7f0bb791a75a
jersoncf <jerson.figueiredo@primeconsulting.org>
2016-10-10 17:34:43 -0100

    Change ems_class model and interface

--- emsprime_core/models/ems_class.py
+++ emsprime_core/models/ems_class.py
@@ -38,10 +38,18 @@ class EmsClass(models.Model):
     name = fields.Char('Class Name', size=64, required=True)
     faculty_id = fields.Many2one('ems.faculty', 'Faculty', required=True)
     subject_id = fields.Many2one('ems.subject', 'Subject', required=True)
-    student_ids = fields.Many2many('ems.student', string='Student(s)')
+    course_id = fields.Many2one('ems.course', 'Course', required=True)
+    #student_ids = fields.Many2many('ems.student', string='Student(s)')
     enrollment_ids = fields.Many2many('ems.enrollment', string='Enrollment(s)')
     #semester_id = fields.Many2one('ems.semester', string='Semester')
     semester = fields.Selection([('I', 'I'), ('II', 'II')], 'Semester')
-    year = fields.Char('Year', size=4)
+    #year = fields.Char('Year', size=4)
+    academic_year = fields.Selection([('2016', '2016/2017'),('2015', '2015/2016'), ('2014', '2014/2015'),('2013', '2013/2014'),('2012', '2012/2013'), 
+                                      ('2011', '2011/2012'), ('2010', '2010/2011'), ('2009', '2009/2010'), ('2008', '2008/2009'), ('2007', '2007/2008'),
+									  ('2006', '2006/2007'), ('2005', '2005/2006'), ('2004', '2004/2005'), ('2003', '2003/2004'),('2002', '2002/2003'),
+									  ('2001', '2001/2002'),('2000', '2000/2001'),('1999', '1999/2000'),('1998', '1998/1999'),('1997', '1997/1998'),
+									  ('1996', '1996/1997'), ('1995', '1995/1996'),('1994', '1994/1995'),('1993', '1993/1994'),('1992', '1992/1993'),
+									  ('1991', '1991/1992')
+            ], 'Academic Year', track_visibility='onchange', required=True)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
