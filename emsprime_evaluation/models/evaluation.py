d923b9d47915d253a7d003079ea55bf12f6131ce
jersoncf <jerson.figueiredo@primeconsulting.org>
2016-11-03 11:44:55 -0100

    Changes to evaluation module

--- emsprime_evaluation/models/evaluation.py
+++ emsprime_evaluation/models/evaluation.py
@@ -25,20 +25,22 @@ from openerp.exceptions import ValidationError
 
 class OpEvaluation(models.Model):
     _name = 'ems.evaluation'
+    _rec_name = 'exam_code'
     _inherit = 'mail.thread'
     _description = 'Exam'
 
-    session_id = fields.Many2one('ems.evaluation.session', 'Exam Session')
+    #session_id = fields.Many2one('ems.evaluation.session', 'Exam Session')
+    faculty_id = fields.Many2one('ems.faculty', string='Faculty', required=True)
     subject_id = fields.Many2one('ems.subject', 'Subject', required=True)
     class_id = fields.Many2one('ems.class', 'Class', required=True)
     exam_code = fields.Char('Exam Code', size=8, required=True)
     exam_type = fields.Many2one('ems.evaluation.type', 'Exam Type', required=True)
-    evaluation_type = fields.Selection(
-        [('normal', 'Normal'), ('GPA', 'GPA'), ('CWA', 'CWA'), ('CCE', 'CCE')],
-        'Evaluation Type', default="normal", required=True)
+    #evaluation_type = fields.Selection(
+    #    [('normal', 'Normal'), ('GPA', 'GPA'), ('CWA', 'CWA'), ('CCE', 'CCE')],
+    #    'Evaluation Type', default="normal", required=True)
     attendees_line = fields.One2many(
-        'ems.evaluation.attendee', 'evaluation_id', 'Attendees', readonly=True)
-    venue = fields.Many2one('res.partner', 'Venue')
+        'ems.evaluation.attendee', 'evaluation_id', 'Attendees')
+    #venue = fields.Many2one('res.partner', 'Venue')
     start_time = fields.Datetime('Start Time', required=True)
     end_time = fields.Datetime('End Time', required=True)
     state = fields.Selection(
@@ -47,10 +49,18 @@ class OpEvaluation(models.Model):
         readonly=True, default='new', track_visibility='onchange')
     note = fields.Text('Note')
     responsible_id = fields.Many2many('ems.faculty', string='Responsible')
-    name = fields.Char('Exam', size=256, required=True)
+    #name = fields.Char('Exam', size=256, required=True)
     total_marks = fields.Float('Total Marks', required=True)
     min_marks = fields.Float('Passing Marks', required=True)
-    room_id = fields.Many2one('ems.evaluation.room', 'Room', required=True)
+    room_id = fields.Many2one('ems.evaluation.room', 'Room')
+    academic_year = fields.Selection([('2016', '2016/2017'),('2015', '2015/2016'), ('2014', '2014/2015'),('2013', '2013/2014'),('2012', '2012/2013'), 
+                                      ('2011', '2011/2012'), ('2010', '2010/2011'), ('2009', '2009/2010'), ('2008', '2008/2009'), ('2007', '2007/2008'),
+									  ('2006', '2006/2007'), ('2005', '2005/2006'), ('2004', '2004/2005'), ('2003', '2003/2004'),('2002', '2002/2003'),
+									  ('2001', '2001/2002'),('2000', '2000/2001'),('1999', '1999/2000'),('1998', '1998/1999'),('1997', '1997/1998'),
+									  ('1996', '1996/1997'), ('1995', '1995/1996'),('1994', '1994/1995'),('1993', '1993/1994'),('1992', '1992/1993'),
+									  ('1991', '1991/1992')
+            ], 'Academic Year', track_visibility='onchange', required=True)
+    university_center_id = fields.Many2one('ems.university.center', 'University Center', required=True)
 
     @api.constrains('total_marks', 'min_marks')
     def _check_marks(self):
